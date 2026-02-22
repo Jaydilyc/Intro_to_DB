4ef466a029d7f9f3a218ff6f5d22bfcaf1400ee6
Oladipo Adeeko <jaydilyc@hotmail.com>
2026-02-22 14:49:33 -0800

    Task 2 File +2

--- task_2.sql
+++ task_2.sql
@@ -18,4 +18,33 @@ CREATE TABLE IF NOT EXISTS Books (
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
         ON UPDATE CASCADE
         ON DELETE RESTRICT
-) ENGINE=INNO
+) ENGINE=INNODB;
+
+CREATE TABLE IF NOT EXISTS Customers (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL,
+    address TEXT
+) ENGINE=INNODB;
+
+CREATE TABLE IF NOT EXISTS Orders (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT NOT NULL,
+    order_date DATE NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT
+) ENGINE=INNODB;
+
+CREATE TABLE IF NOT EXISTS Order_Details (
+    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT NOT NULL,
+    book_id INT NOT NULL,
+    quantity DOUBLE NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT
+) ENGINE=INNODB;
